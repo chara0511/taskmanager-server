7348aa7bb111314e2b28c2cbe4287d670ca473b2
Chara- <escor05_11@hotmail.com>
2020-06-07 21:52:30 -0500

    mongoDB connected

---
+++ config/database.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+
+require("dotenv").config({ path: "variables.env" });
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.DB_MONGO, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false,
+    });
+    console.log("DB Connected!!!");
+  } catch (error) {
+    console.log(error);
+    process.exit(1); // stop app
+  }
+};
+
+module.exports = connectDB;
--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require("express");
+const connectDB = require("./config/database");
 
 // Create server
 const app = express();
+
+// Connect to DB
+connectDB();
 
 // App port
 const PORT = process.env.PORT || 4000;
