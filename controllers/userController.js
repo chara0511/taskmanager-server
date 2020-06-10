b9496b6b7661348fa292f1132dd17772d1ac56e1
Chara- <escor05_11@hotmail.com>
2020-06-10 00:20:01 -0500

    inserting users to BD

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,3 +1,20 @@
-exports.createUser = (req, res) => {
-  console.log(req.body);
+const User = require("../models/User");
+
+exports.createUser = async (req, res) => {
+  try {
+    let user;
+
+    // create a new user
+    user = new User(req.body);
+
+    // save a user
+    await user.save();
+
+    // message
+    res.send("user created");
+  } catch (error) {
+    console.log(error);
+
+    res.status(400).send("something went wrong");
+  }
 };
