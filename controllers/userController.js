581431450ea28efbdc8297e7974df50d33e1cc8a
Chara- <escor05_11@hotmail.com>
2020-06-11 23:04:30 -0500

    JWT created

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,10 +1,12 @@
 const User = require("../models/User");
 const bcryptjs = require("bcryptjs");
 const { validationResult } = require("express-validator");
+const jwt = require("jsonwebtoken");
 
 exports.createUser = async (req, res) => {
   // Check validation errors
   const errors = validationResult(req);
+
   if (!errors.isEmpty()) {
     return res.status(400).json({ errors: errors.array() });
   }
@@ -30,8 +32,25 @@ exports.createUser = async (req, res) => {
     // save a user
     await user.save();
 
-    // message
-    res.json({ msg: "User created" });
+    // create JWT
+    const payload = {
+      user: { id: user.id },
+    };
+
+    // sign JWT
+    jwt.sign(
+      payload,
+      process.env.SECRET,
+      {
+        expiresIn: 3600,
+      },
+      (error, token) => {
+        if (error) throw error;
+
+        // message
+        res.json({ token: token });
+      }
+    );
   } catch (error) {
     console.log(error);
 
