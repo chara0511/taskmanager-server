07e4340a05b31a4e7a8d80a371409c28af4810f1
Chara- <escor05_11@hotmail.com>
2020-06-15 22:31:37 -0500

    update project by id success

--- controllers/projectController.js
+++ controllers/projectController.js
@@ -41,7 +41,7 @@ exports.getProjects = async (req, res) => {
 
 // Update a project
 exports.updateProject = async (req, res) => {
-  // Check validation errors
+  // check validation errors
   const errors = validationResult(req);
 
   if (!errors.isEmpty()) {
@@ -49,9 +49,37 @@ exports.updateProject = async (req, res) => {
   }
   // extract project information
   const { name } = req.body;
+
   const newProject = {};
+
+  if (name) {
+    newProject.name = name;
+  }
+
   try {
+    // check id
+    let project = await Project.findById(req.params.id);
+
+    // check if project exists
+    if (!project) {
+      return res.status(404).json({ msg: "Project not found" });
+    }
+
+    // check project owner
+    if (project.owner.toString() !== req.user.id) {
+      return res.status(401).json({ msg: "Not authorized" });
+    }
+
+    // update
+    project = await Project.findByIdAndUpdate(
+      { _id: req.params.id },
+      { $set: newProject },
+      { new: true }
+    );
+
+    res.json({ project });
   } catch (error) {
     console.log(error);
+    res.status(500).send("Server error");
   }
 };
