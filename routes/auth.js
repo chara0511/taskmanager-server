3d8f374742c4211d18f5e3aaad9c6b889b9ea71c
Chara- <escor05_11@hotmail.com>
2020-06-23 09:12:08 -0500

    validate login

--- routes/auth.js
+++ routes/auth.js
@@ -6,16 +6,7 @@ const authController = require("../controllers/authController");
 const auth = require("../middlewares/auth");
 
 // Log in user -> api/auth
-router.post(
-  "/",
-  [
-    check("email", "Add a valid email").isEmail(),
-    check("password", "The password must be at least 6 characters").isLength({
-      min: 6,
-    }),
-  ],
-  authController.authUser
-);
+router.post("/", authController.authUser);
 
 // Get auth user
 router.get("/", auth, authController.authenticatedUser);
